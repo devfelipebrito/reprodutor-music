22b96880cbebc3d4c6b6180367c131bc50cb3fb5
Felipe Brito <felipebrito.dev@gmail.com>
2023-09-27 22:04:27 -0300

    button-shunffle

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const cover =document.getElementById( 'cover' );
 const play = document.getElementById( 'play' );
 const previous = document.getElementById( 'previous' );
 const next = document.getElementById( 'next' );
+const currentProgress = document.getElementById( 'current-progress' );
+const progressContainer = document.getElementById( 'progress-container' );
+const shuffleButton = document.getElementById( 'shuffle' );
 
 const gatinhaAssanhada = {
     songName: 'Gatinha Assanhada',
@@ -25,7 +28,10 @@ const elaQuerDancar = {
 };
 
 let isPlaying = false;
-const playList = [gatinhaAssanhada, seSolta, elaQuerDancar];
+let isShuffled = false;
+
+const originalPlayList = [gatinhaAssanhada, seSolta, elaQuerDancar];
+let sortedPlayList = [...originalPlayList];
 let index = 0;
 
 function playSong() {
@@ -52,15 +58,15 @@ function playPauseDecider() {
 }
 
 function initializeSong() {
-    cover.src = `assets/${playList[index].file}.jpg`;
-    song.src = `songs/${playList[index].file}.mp3`;
-    songName.innerText = playList[index].songName;
-    bandName.innerText =playList[index].artist;
+    cover.src = `assets/${sortedPlayList[index].file}.jpg`;
+    song.src = `songs/${sortedPlayList[index].file}.mp3`;
+    songName.innerText = sortedPlayList[index].songName;
+    bandName.innerText =sortedPlayList[index].artist;
 }
 
 function previousSong() {
     if(index === 0 ){
-        index = playList.length -1;
+        index = sortedPlayList.length -1;
     }
     else{
         index -= 1;
@@ -70,7 +76,7 @@ function previousSong() {
 }
 
 function nextSong() {
-    if(index === playList.length -1){
+    if(index === sortedPlayList.length -1){
         index = 0;
     }
     else{
@@ -80,8 +86,50 @@ function nextSong() {
     playSong();
 }
 
+function updateProgressBar() {
+    song.currentTime
+    song.duration
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+function jumpTo(event) {
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)*song.duration;
+    song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray) {
+    const size = preShuffleArray.length;
+    let currentIndex = size - 1;
+    while (currentIndex > 0){
+        let randomIndex = Math.floor(Math.random()*size);
+        let aux = preShuffleArray[currentIndex];
+        preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+        preShuffleArray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
+
+function shuffleButtonClicked() {
+    if(isShuffled === false){
+        isShuffled = true;
+        shuffleArray(sortedPlayList);
+        shuffleButton.classList.add('button-active');
+    }
+    else {
+        isShuffled = false;
+        sortedPlayList = [...originalPlayList];
+        shuffleButton.classList.remove('button-active');
+    }
+}
+
 initializeSong();
 
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
